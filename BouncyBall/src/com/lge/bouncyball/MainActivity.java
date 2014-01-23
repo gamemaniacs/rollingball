898446d924efc830b3622f2f6d8d3407d20b14be
Mitesh Patel <miteshpatel.tech@gmail.com>
2014-01-23 12:47:42 +0530

    Added Concept View of Main Activity Added Ball Image created from Inkscape GameLoop Code added in Main Activity

--- BouncyBall/src/com/lge/bouncyball/MainActivity.java
+++ BouncyBall/src/com/lge/bouncyball/MainActivity.java
@@ -1,22 +1,47 @@
 package com.lge.bouncyball;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements Runnable{
 
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
 
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		while(true) {
+			gameLoop();
+		}
+		
+	}
+	
+	/**
+	 * Main loop for screen update.
+	 * This function should only be called from a separate thread and within a continuous While loop
+	 */
+	private void gameLoop(){
+		
 	}
 
 }
